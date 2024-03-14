f7c1f0500eeebaadd90d3fde7f3940afd7c53a24
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2024-03-14 16:12:44 +0800

    chore: fix button can't have className

--- components/button.tsx
+++ components/button.tsx
@@ -1,15 +1,17 @@
 import styles from './button.module.css'
 import { motion } from 'framer-motion'
 import { ReactNode } from 'react'
+import $ from 'clsx'
 
 interface ButtonParam {
   children: ReactNode,
+  className?: string,
   [key: string]: unknown
 }
 
-export default function Button({ children, ...rest }: ButtonParam) {
+export default function Button({ children, className, ...rest }: ButtonParam) {
   return (
-    <motion.div className={styles.button} {...rest}>
+    <motion.div className={$(styles.button, className)} {...rest}>
       {children}
     </motion.div>
   )
