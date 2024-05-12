95058dccae6f73c882f68d867bcc55cebe53c8ec
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2024-05-13 05:30:12 +0800

    feature: add the bonus I level

---
+++ app/levels/6/page.tsx
@@ -0,0 +1,9 @@
+export default async function BonusUno() {
+  return (
+    <>
+      <h1>¿Qué es?</h1>
+      <p>¿Qué es Khinzeniollo?</p>
+      <p>Khinzeniollo es una palabra inventada. ¿Cuál es su origen?</p>
+    </>
+  )
+}
