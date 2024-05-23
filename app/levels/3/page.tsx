e6764dc9a80ab0156ba2d04bee40b20b91d28d7c
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2024-05-23 17:17:05 +0800

    feature: full level 3

--- app/levels/3/page.tsx
+++ app/levels/3/page.tsx
@@ -4,7 +4,8 @@ import { useEffect } from 'react';
 
 const SIZE = 600;
 const RADIUS = 10;
-const VELOCITY = 5;
+const VELOCITY = 10;
+const DELTA = RADIUS - VELOCITY;
 
 export default function Tres() {
 
@@ -30,17 +31,25 @@ enum WallMode {
   Through, Block, Mirror
 }
 
-const ball = { x: SIZE / 4 + RADIUS, y: RADIUS, vx: VELOCITY, vy: VELOCITY, r: RADIUS };
+const ball = { x: SIZE / 4 + RADIUS, y: RADIUS, vx: -VELOCITY, vy: -VELOCITY, r: RADIUS };
 const walls: Record<"up" | "down" | "left" | "right", { from: [number, number], to: [number, number], mode: WallMode }> = {
-  up: { from: [SIZE / 2, 0], to: [SIZE / 2, SIZE / 3], mode: WallMode.Through }, 
-  left: { from: [0, SIZE / 2], to: [SIZE / 3, SIZE / 2], mode: WallMode.Through }, 
+  up:    { from: [SIZE / 2, 0],    to: [SIZE / 2, SIZE / 3],     mode: WallMode.Through }, 
+  left:  { from: [0, SIZE / 2],    to: [SIZE / 3, SIZE / 2],     mode: WallMode.Through }, 
   right: { from: [SIZE, SIZE / 2], to: [SIZE * 2 / 3, SIZE / 2], mode: WallMode.Through }, 
-  down: { from: [SIZE / 2, SIZE], to: [SIZE / 2, SIZE * 2 / 3], mode: WallMode.Through }, 
+  down:  { from: [SIZE / 2, SIZE], to: [SIZE / 2, SIZE * 2 / 3], mode: WallMode.Through }, 
 } as const;
+const targets = [
+  [{ x: SIZE     / 4 + DELTA, y: DELTA }       , { x: DELTA       , y: SIZE     / 4 + DELTA }], 
+  [{ x: SIZE * 3 / 4 - DELTA, y: DELTA }       , { x: SIZE - DELTA, y: SIZE     / 4 + DELTA }], 
+  [{ x: SIZE     / 4 + DELTA, y: SIZE - DELTA }, { x: DELTA       , y: SIZE * 3 / 4 - DELTA }], 
+  [{ x: SIZE * 3 / 4 - DELTA, y: SIZE - DELTA }, { x: SIZE - DELTA, y: SIZE * 3 / 4 - DELTA }], 
+]
+
+let target = structuredClone(targets[0]);
+let lastTarget = 0;
 let shifting = false;
 
 function onKeyDown(key: string) {
-  console.log(key)
   switch (key) {
     case 'w': case 'W':
       walls.up.mode = shifting ? WallMode.Mirror : WallMode.Block;
@@ -65,7 +74,6 @@ function onKeyDown(key: string) {
 }
 
 function onKeyUp(key: string) {
-  console.log("")
   switch (key) {
     case 'w': case "W":
       walls.up.mode = WallMode.Through;
@@ -96,12 +104,17 @@ function animate(ctx: CanvasRenderingContext2D) {
   for (const wall of Object.values(walls)) {
     ctx.strokeStyle = 
       wall.mode === WallMode.Through ? "green" : 
-      wall.mode === WallMode.Block ? "red" : "purple"
+      wall.mode === WallMode.Block ? "red" : "blue"
     ctx.beginPath();
     ctx.moveTo(...wall.from);
     ctx.lineTo(...wall.to);
     ctx.stroke();
   }
+
+  ctx.fillStyle = "purple"
+  ctx.beginPath()
+  ctx.arc(target[0].x, target[0].y, RADIUS, 0, 2 * Math.PI, false)
+  ctx.fill()
 
   ctx.fillStyle = "white"
   ctx.beginPath()
@@ -111,12 +124,37 @@ function animate(ctx: CanvasRenderingContext2D) {
   ball.x += ball.vx;
   ball.y += ball.vy;
 
+  let collide = false;
   if (ball.x < RADIUS || SIZE - RADIUS < ball.x) {
     ball.vx = -ball.vx;
+    collide = true;
   }
   if (ball.y < RADIUS || SIZE - RADIUS < ball.y) {
     ball.vy = -ball.vy;
+    collide = true;
   }
+  
+  if (collide) {
+    if (ball.x === target[0].x && ball.y === target[0].y) {
+      target.shift();
+      if (target.length === 0) {
+        let r = lastTarget
+        while (r === lastTarget) {
+          r = Math.floor(Math.random() * targets.length);
+        }
+        let [a, b] = targets[r];
+        if (Math.random() > 0.5) {
+          [a, b] = [b, a];
+        }
+        lastTarget = r;
+        target = [a, b];
+      }
+    }
+    else {
+      // alert("You suck");
+    }
+  }
+
 
   if (ball.x === SIZE / 2) {
     if (walls.up.mode === WallMode.Block && ball.y < SIZE / 2 || walls.down.mode === WallMode.Block && ball.y > SIZE / 2) {
