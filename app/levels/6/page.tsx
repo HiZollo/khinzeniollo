e9bafa7238024d66d18cc19c0d42eef3dee7005f
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2024-05-26 08:29:21 +0800

    chore: change title of bonus i

--- app/levels/6/page.tsx
+++ app/levels/6/page.tsx
@@ -1,7 +1,7 @@
 export default async function BonusUno() {
   return (
     <>
-      <h1>¿Qué es?</h1>
+      <h1>¿Cuál es?</h1>
       <p>¿Qué es Khinzeniollo?</p>
       <p>Khinzeniollo es una palabra inventada. ¿Cuál es su origen?</p>
     </>
