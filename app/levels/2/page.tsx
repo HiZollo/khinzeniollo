549c326c26293bfee085303d778f24ab1ee6944a
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2024-05-29 06:07:01 +0800

    chore: fix this damn issue

--- app/levels/2/page.tsx
+++ app/levels/2/page.tsx
@@ -5,6 +5,7 @@ import { createCanvas, loadImage } from 'canvas'
 import Image from 'next/image'
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from '@/lib/auth'
+import fs from 'node:fs'
 
 // const originalText = `Iam illa, perfugia quae sumunt sibi ad excusationem quo facilius otio perfruantur, certe minime sunt audienda, cum ita dicunt accedere ad rem publicam plerumque homines nulla re bona dignos, cum quibus comparari sordidum, confligere autem multitudine praesertim incitata miserum et periculosum sit. quam ob rem neque sapientis esse accipere habenas cum insanos atque indomitos impetus volgi cohibere non possit, neque liberi cum inpuris atque inmanibus adversariis decertantem vel contumeliarum verbera subire, vel expectare sapienti non ferendas iniurias: proinde quasi bonis et fortibus et magno animo praeditis ulla sit ad rem publicam adeundi causa iustior, quam ne pareant inprobis, neve ab isdem lacerari rem publicam patiantur, cum ipsi auxilium ferre si cupiant non queant.
 // Illa autem exceptio cui probari tandem potest, quod negant sapientem suscepturum ullam rei publicae partem, extra quam si eum tempus et necessitas coegerit? quasi vero maior cuiquam necessitas accidere possit quam accidit nobis; in qua quid facere potuissem, nisi tum consul fuissem? consul autem esse qui potui, nisi eum vitae cursum tenuissem a pueritia, per quem equestri loco natus pervenirem ad honorem amplissimum? non igitur potestas est ex tempore aut cum velis opitulandi rei publicae, quamvis ea prematur periculis, nisi eo loco sis ut tibi id facere liceat.`
@@ -27,8 +28,6 @@ const LINE_HEIGHT = 30
 const CANVAS_WIDTH_HALF = CANVAS_WIDTH / 2
 
 export default async function Dos() {
-  console.log(process.cwd())
-  console.log(__dirname)
   const session = (await getServerSession(authOptions))!
 
   const index = getSectionIndex(session.user.id!)
@@ -47,7 +46,8 @@ export default async function Dos() {
 
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  const slate = await loadImage(process.cwd() + '/public/slate_background_2.png')
+  const slate = await loadImage('https://files.catbox.moe/ezp974.png')
+  // const slate = await loadImage('https://khinzeniollo.vercel.app/slate_background_2.png')
   const background = ctx.createPattern(slate, 'repeat')
   ctx.fillStyle = background
   ctx.fillRect(0, 0, canvas.width, canvas.height)
@@ -72,7 +72,8 @@ export default async function Dos() {
   })
 
   ctx.translate(-300, -canvas.height-35)
-  const crack = await loadImage(process.cwd() + '/public/crack.png')
+  const crack = await loadImage('https://files.catbox.moe/wsmgp7.png')
+  // const crack = await loadImage('https://khinzeniollo.vercel.app/crack.png')
   const crack_effect = ctx.createPattern(crack, 'repeat')
   ctx.fillStyle = crack_effect
   ctx.fillRect(0, 0, canvas.width, canvas.height)
